5cd032bcc8e6261335055001c664e8fc0e297678
DESKTOP-CFOLVV6\antho <anthonybarman2003@gmail.com>
2025-08-16 19:44:18 +0200

    End first 3C

---
+++ Assets/Trijeux Dossier/Script/TPDeadZone.cs
@@ -0,0 +1,39 @@
+// Script by : Trijeux
+// Porject : PJPJam
+
+using System;
+using UnityEngine;
+
+public class TPDeadZone : MonoBehaviour
+{
+    #region Attributs
+
+    [SerializeField] private GameObject tpZone;
+    [SerializeField] private string playerTag;
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+
+    #region InputSystem
+
+
+
+    #endregion
+
+    #region Behaviors
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            other.gameObject.transform.position = tpZone.transform.position;
+        }
+    }
+
+    #endregion
+}
