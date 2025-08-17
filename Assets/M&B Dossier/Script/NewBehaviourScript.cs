84238fba08cfe1dc664ff4a35bf29e0b2f9e8858
hugo zeidan <hugzeidan@outlook.com>
2025-08-17 06:20:19 +0200

    merge fix

---
+++ Assets/M&B Dossier/Script/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+// Script by : Mute&Blind 
+// Porject : PJPJam
+
+using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    #region Attributs
+
+
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+
+    #region InputSystem
+
+
+
+    #endregion
+
+    #region Behaviors
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+	#endregion
+}
